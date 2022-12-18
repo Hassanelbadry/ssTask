87d7cafb065d9790dcbb8dd84d1d2febfcd95f43
Hassanelbadry <117593150+Hassanelbadry@users.noreply.github.com>
2022-12-18 20:47:18 +0200

    Add files via upload

---
+++ app/tempCodeRunnerFile.cs
@@ -0,0 +1,9 @@
+            // int c = 0;
+            // for (int i =0 ; i < b.length ; i++)
+            // {
+            //     if (b[i] ='a')
+            //     {
+            //         c++;
+            //     }
+            // }
+            // Console.Write(c);
