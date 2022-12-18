3ee991ba554c833fd859ad64945b2be2188c6550
Hassanelbadry <117593150+Hassanelbadry@users.noreply.github.com>
2022-12-18 20:48:24 +0200

    Delete tempCodeRunnerFile.cs

--- app/tempCodeRunnerFile.cs
+++
@@ -1,9 +0,0 @@
-            // int c = 0;
-            // for (int i =0 ; i < b.length ; i++)
-            // {
-            //     if (b[i] ='a')
-            //     {
-            //         c++;
-            //     }
-            // }
-            // Console.Write(c);
